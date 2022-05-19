e0623f0f5711ad1c3ba634e85f5134d284ab4208
VicenteSaldana1 <vsaldana210@gmail.com>
2022-05-19 10:54:03 -0700

    Completed tasks for index and added apartments to seed file.

--- app/javascript/components/pages/ApartmentIndex.js
+++ app/javascript/components/pages/ApartmentIndex.js
@@ -1,8 +1,33 @@
 import React, { Component } from "react";
+import { Card, CardTitle, Col } from "reactstrap";
 
 class ApartmentIndex extends Component {
   render() {
-    return <h3>Apartment Listings</h3>;
+    return (
+      <>
+        <h3>ApartmentIndex</h3>
+        <Col sm="6">
+          {this.props.apartments &&
+            this.props.apartments.map((apartment) => {
+              return (
+                <Card body key={apartment.id}>
+                  <CardTitle>
+                    <h4>Street:</h4>
+                    <h4>{apartment.street}</h4>
+                    <h4>City:</h4>
+                    <h4>{apartment.city}</h4>
+                    <h4>Manager:</h4>
+                    <h4>{apartment.manager}</h4>
+                    <h4>Bedrooms:</h4>
+                    <h4>{apartment.bedrooms}</h4>
+                    <img src={apartment.image} width="200px" />
+                  </CardTitle>
+                </Card>
+              );
+            })}
+        </Col>
+      </>
+    );
   }
 }
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.where(email: 'test@test.test').first_or_create(password: '12345678', password_confirmation: '12345678')
+
+apartment = [
+  {
+    street: '5590 Baltimore Drive', 
+    city: 'La Mesa', 
+    state: 'California',
+    manager: 'Lebron James',
+    email: 'lebron@gmail.com',
+    price: '$5000',
+    bedrooms: 2, 
+    bathrooms: 2,
+    pets: 'Yes',
+    image: 'https://cdngeneral.rentcafe.com/dmslivecafe/2/102601/THEA%20at%20Metropolis%20Two%20Bedroom%20Living%20Area.jpg?crop=(0,0,300,200)&cropxunits=300&cropyunits=200&width=380',
+    user_id: 1
+  },
+  {
+    street: '6690 Baltimore Drive', 
+    city: 'La Mesa', 
+    state: 'California',
+    manager: 'Kobe Bean Bryant',
+    email: 'kobe@gmail.com',
+    price: '$6000',
+    bedrooms: 3, 
+    bathrooms: 3,
+    pets: 'Yes',
+    image: 'https://images1.apartments.com/i2/Mel4Hm5GU_A13jVZId0JNpI5il1Qshvtiie1_5bXZhY/117/image.jpg',
+    user_id: 1
+  },
+  {
+    street: '7589 Baltimore Drive', 
+    city: 'La Mesa', 
+    state: 'California',
+    manager: 'Kobe Bean Bryant',
+    email: 'kobe@gmail.com',
+    price: '$6000',
+    bedrooms: 3, 
+    bathrooms: 3,
+    pets: 'Yes',
+    image: 'https://images1.apartments.com/i2/Mel4Hm5GU_A13jVZId0JNpI5il1Qshvtiie1_5bXZhY/117/image.jpg',
+    user_id: 1
+  },
+]
+
+apartment.each do |each_apartment|
+  user.apartments.create each_apartment
+  puts "creating apartment #{each_apartment}"
+end
