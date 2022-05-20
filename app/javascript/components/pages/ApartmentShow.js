e0d7d8eca43bfbe4d0c0427650bfb1e8529d816d
VicenteSaldana1 <vsaldana210@gmail.com>
2022-05-19 17:04:09 -0700

    completed unprotected show tasks, added some styling to 'show' page, and updated show.test page.

--- app/javascript/components/pages/ApartmentShow.js
+++ app/javascript/components/pages/ApartmentShow.js
@@ -1,9 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-export default class ApartmentShow extends Component {
+class ApartmentShow extends Component {
   render() {
+    let { apartment } = this.props 
     return (
-      <h3>ApartmentShow</h3>
-    )
+      <>
+      <h3>Beautiful Apartment Available</h3>
+      {apartment &&
+      <div>
+        <p>Street: {apartment.street}</p>
+        <p>City: {apartment.city}</p>
+        <p>State: {apartment.state}</p>
+        <p>Price: {apartment.price}</p>
+        <p>Bedrooms: {apartment.bedrooms}</p>
+        <p>Bathrooms: {apartment.bathrooms}</p>
+        <p>Pets: {apartment.pets}</p>
+        <p>Manager: {apartment.manager}</p>
+        <p>Manager Email: {apartment. email}</p>
+        <img src={apartment.image} width="400px" />
+      </div>
+      }
+    </>)
   }
 }
+ 
+export default ApartmentShow;
